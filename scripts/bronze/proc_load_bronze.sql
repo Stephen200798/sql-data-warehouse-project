082e317bf92510e91d91084480bc0fa4b5fd73e4
Stephen Akintunde Akinfe <akinfestephen52@gmail.com>
2026-01-10 13:47:42 +0100

    Update file paths for bulk inserts in proc_load_bronze

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -1,3 +1,4 @@
+
 /*
 =======================================================================================
 Stored Procedure: Load Bronze Layer (Source -> Bronze)
@@ -35,7 +36,7 @@ BEGIN
 
 		Print '>> Inserting Data Into: bronze.crm_cust_info'
 		bulk insert bronze.crm_cust_info 
-		from 'C:\sql\dwh_project\datasets\source_crm\cust_info.csv'
+		from 'C:\Users\User\Downloads\sql\dwh_project\datasets\source_crm\cust_info.csv'
 		with (
 			firstrow = 2,
 			fieldterminator = ',',
@@ -51,7 +52,7 @@ BEGIN
 
 		Print '>> Inseting Data Into: bronze.crm_prd_info'
 		bulk insert bronze.crm_prd_info
-		from 'C:\sql\dwh_project\datasets\source_crm\prd_info.csv'
+		from 'C:\Users\User\Downloads\sql\dwh_project\datasets\source_crm\prd_info.csv'
 		with ( 
 			firstrow = 2,
 			fieldterminator = ',',
@@ -67,7 +68,7 @@ BEGIN
 
 		Print '>> Inserting Data Into: bronze.crm_sales_details'
 		bulk insert bronze.crm_sales_details
-		from 'C:\sql\dwh_project\datasets\source_crm\sales_details.csv'
+		from 'C:\Users\User\Downloads\sql\dwh_project\datasets\source_crm\sales_details.csv'
 		with (
 			firstrow = 2,
 			fieldterminator = ',',
@@ -87,7 +88,7 @@ BEGIN
 
 		Print '>> Inserting Data Into: bronze.erp_cust_az12'
 		bulk insert bronze.erp_cust_az12
-		from 'C:\sql\dwh_project\datasets\source_erp\CUST_AZ12.csv'
+		from 'C:\Users\User\Downloads\sql\dwh_project\datasets\source_erp\CUST_AZ12.csv'
 		with (
 			firstrow = 2, 
 			fieldterminator = ',',
@@ -103,7 +104,7 @@ BEGIN
 
 		Print '>> Inserting Data Into: bronze.erp_loc_a101'
 		bulk insert bronze.erp_loc_a101
-		from 'C:\sql\dwh_project\datasets\source_erp\LOC_A101.csv'
+		from 'C:\Users\User\Downloads\sql\dwh_project\datasets\source_erp\LOC_A101.csv'
 		with ( 
 			firstrow = 2,
 			fieldterminator = ',',
@@ -119,7 +120,7 @@ BEGIN
 
 		Print '>> Inserting Data Into: bronze.erp_px_cat_g1v2'
 		bulk insert bronze.erp_px_cat_g1v2
-		from 'C:\sql\dwh_project\datasets\source_erp\PX_CAT_G1V2.csv'
+		from 'C:\Users\User\Downloads\sql\dwh_project\datasets\source_erp\PX_CAT_G1V2.csv'
 		with (
 			firstrow = 2,
 			fieldterminator = ',',
